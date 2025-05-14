8a7cc97a5131be48a1cd0b56982ce14589782cd0
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-05-14 16:27:52 +0200

    Pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './styles/_global.scss';
 import AppRoutes from './router/Routes';
-import  store  from './redux/store';  
+import  store  from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/OnlyPhotos/"> {/* Añade basename aquí */}
       <AppRoutes />
     </BrowserRouter>
   </Provider>
